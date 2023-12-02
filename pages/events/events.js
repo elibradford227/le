9dbbd4d1b3b13b956531026ed713bfcef60b1166
Eli Bradford <elibradford227@gmail.com>
2023-12-02 14:05:09 -0600

    leave and join working

--- pages/events/events.js
+++ pages/events/events.js
@@ -1,17 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/router';
 import EventCard from '../../components/event/eventCard';
-import { getEvents } from '../../utils/data/eventData';
+import { getEvents, leaveEvent, joinEvent } from '../../utils/data/eventData';
+import { useAuth } from '../../utils/context/authContext';
 
 function Home() {
   const [events, setEvents] = useState([]);
 
+  const user = useAuth();
+
   const router = useRouter();
 
-  useEffect(() => {
-    getEvents().then((data) => setEvents(data));
+  const leaveEventFunc = useCallback((eventId, userId) => {
+    console.warn(eventId, userId);
+    leaveEvent(eventId, userId).then(router.reload);
   }, []);
+
+  const joinEventFunc = useCallback((eventId, uid) => {
+    joinEvent(eventId, uid).then(router.reload);
+  }, []);
+
+  useEffect(() => {
+    getEvents(user.user.uid).then((data) => setEvents(data));
+  }, [user.user.uid]);
 
   return (
     <article className="events">
@@ -24,9 +36,26 @@ function Home() {
         Register New Event
       </Button>
       {events.map((event) => (
-        <section key={`event--${event.id}`} className="event">
-          <EventCard id={event.id} title={event.title} description={event.description} numberOfPlayers={event.number_of_players} skillLevel={event.skill_level} />
-        </section>
+        event.joined
+          ? (
+            <section key={`event--${event.id}`} className="event">
+              <EventCard id={event.id} title={event.title} description={event.description} numberOfPlayers={event.number_of_players} skillLevel={event.skill_level} />
+              <Button variant="danger" onClick={() => leaveEventFunc(event.id, user.user.uid)}>
+                Leave Event
+              </Button>
+            </section>
+          )
+          : (
+            <section key={`event--${event.id}`} className="event">
+              <EventCard id={event.id} title={event.title} description={event.description} numberOfPlayers={event.number_of_players} skillLevel={event.skill_level} />
+              <Button variant="danger" onClick={() => joinEventFunc(event.id, user.user.uid)}>
+                Join Event
+              </Button>
+            </section>
+          )
+        // <section key={`event--${event.id}`} className="event">
+        //   <EventCard id={event.id} title={event.title} description={event.description} numberOfPlayers={event.number_of_players} skillLevel={event.skill_level} />
+        // </section>
       ))}
     </article>
   );
